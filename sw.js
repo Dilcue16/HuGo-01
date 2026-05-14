aedc55aa7f624de2462aff4205664adc89fdd889
Dilcue16 <euclid25280@gmail.com>
2026-05-14 16:30:29 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+// SW KILL — unregister self and clear all caches
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.claim())
+  );
+});
